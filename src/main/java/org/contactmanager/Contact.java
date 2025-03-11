cbc9af25a7de9dd894c2d8905e562503486318f1
ThanujBuddhika <thanujbuddhika02@gmail.com>
2025-03-10 22:55:29 -0700

    Create Dto Class

--- src/main/java/org/contactmanager/Contact.java
+++ src/main/java/org/contactmanager/Contact.java
@@ -1,4 +1,34 @@
 package org.contactmanager;
 
 public class Contact {
+
+    String name;
+    String mobile;
+    String email;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
